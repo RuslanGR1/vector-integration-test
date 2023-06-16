2ea047c0ffb7784014328b6e941c3a8ab3eb0577
Gazizov Ruslan <almetfly@gmail.com>
2023-06-16 11:49:04 +0300

    feat: add initial auth form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vector-request';
+  vectorUsername = '';
+  vectorPassword = '';
+  vectorUrl = 'https://stagevectorapi.edgeestimator.com';
+  tenantId = 'd1372053-e8c4-ed11-9109-ac1f6b0a9f7b';
+
+  access_token = '';
+
+  constructor(private readonly http: HttpClient) {}
+
+  async onLoginClick(): Promise<void> {
+    const vectorBase = 'https://stagevectorapi.edgeestimator.com';
+    const loginUrl = `${vectorBase}/Token`;
+
+    let body = new URLSearchParams();
+    body.set('username', this.vectorUsername);
+    body.set('password', this.vectorPassword);
+    body.set('grant_type', 'password');
+
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+
+    const response = await this.http.post<{ access_token: string }>(loginUrl, body.toString(), options).toPromise();
+    if (!response) {
+      return;
+    }
+    this.access_token = response.access_token;
+
+    this.getBidList();
+  }
+
+  async getBidList(): Promise<void> {
+    const url = `https://stagevectorapi.edgeestimator.com/api/bidprep/list/d1372053-e8c4-ed11-9109-ac1f6b0a9f7b`;
+    let options = {
+      headers: new HttpHeaders().set('authorization', `Bearer ${this.access_token}`)
+    };
+
+    const response = await this.http.post(url, undefined, options).toPromise();
+    console.log('response', response);
+  }
 }
